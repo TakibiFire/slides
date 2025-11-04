76bdb6180aa4910ccf0a2d35f2a5d37f8d5e8ba3
TakibiFire <takibi3fire+github@gmail.com>
2025-11-04 21:43:48 +0900

    Update transformative/global-bottom.vue to load vue files under bottoms.

--- bottoms/blured-donuts.vue
+++ bottoms/blured-donuts.vue
@@ -279,13 +279,6 @@ const circles3 = useCircleSequence(3)
       </defs>
     </svg>
   </div>
-
-  <footer
-    v-if="currentLayout !== 'cover'"
-    class="absolute bottom-0 left-0 right-0 p-2"
-  >
-    {{ currentPage }} / {{ total }}
-  </footer>
 </template>
 
 <style scoped>
---
+++ bottoms/page-total.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useNav } from '@slidev/client';
+
+const { currentLayout, currentPage, total } = useNav()
+</script>
+
+<template>
+  <footer
+    v-if="currentLayout !== 'cover'"
+    class="absolute bottom-0 left-0 right-0 p-2"
+  >
+    {{ currentPage }} / {{ total }}
+  </footer>
+</template>
