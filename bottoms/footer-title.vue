193dce90a92b97f6fed457683050bb8e4d88b520
TakibiFire <takibi3fire+github@gmail.com>
2025-11-06 14:26:34 +0900

    Add footer-title.vue to show footerTitle config value

---
+++ bottoms/footer-title.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useNav, useSlideContext } from '@slidev/client';
+
+const { currentPage } = useNav()
+const { $slidev } = useSlideContext();
+</script>
+
+<template>
+  <footer
+    v-if="currentPage !== 1"
+    class="absolute bottom-0 right-0 p-2 text-gray-600"
+  >
+    {{ $slidev.configs.footerTitle }}
+  </footer>
+</template>
