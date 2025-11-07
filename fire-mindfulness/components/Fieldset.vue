6a7641e6148d34a97592e6c23bb4033fba6bbe23
TakibiFire <takibi3fire+github@gmail.com>
2025-11-07 16:12:38 +0900

    Delete fire-mindfulness until it becomes necessary

--- fire-mindfulness/components/Fieldset.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <fieldset class="fieldset rounded-lg bg-base-200 shadow border-base-300 rounded-box border p-4 bg-gray-100/100">
-    <legend class="fieldset-legend">考え方</legend>
-  </fieldset>
-</template>
